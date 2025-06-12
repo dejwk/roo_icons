172bde2b227e8ff16528179181638e0ae116a1d4
dejwk <dawid.kurzyniec@gmail.com>
2025-06-12 23:13:15 +0200

    Fix: do not use the library pre-compiled on Xtensa, for ESP32C3 which uses a RISC CPU.

--- prebuild.py
+++ prebuild.py
@@ -2,8 +2,15 @@ Import('env')
 from os.path import join, realpath
 
 for item in env.get("CPPDEFINES", []):
+    is_esp32 = False
     if item == "ESP32":
-        env.Replace(SRC_FILTER=["-<*>"])
-        env.Append(LIBPATH=[realpath("src/esp32")])
-        env.Append(LIBS=["-lroo_icons"])
+        is_esp32 = True
+
+    if item == "ARDUINO_ESP32C3_DEV":
+        is_esp32 = False
         break
+
+if is_esp32:
+    env.Replace(SRC_FILTER=["-<*>"])
+    env.Append(LIBPATH=[realpath("src/esp32")])
+    env.Append(LIBS=["-lroo_icons"])
